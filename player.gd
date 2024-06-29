c8951c5fa8671bf727728c8038a384a6423db985
ilyich122 <140055029+ilyich122@users.noreply.github.com>
2024-06-29 12:34:51 +0300

    0,003
    добавил папки немного привел в порядок структуру создал примерные сены противников\союзников

--- player.gd
+++ player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody3D
 
 # How fast the player moves in meters per second.
-@export var speed = 14
+@export var speed = 40
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+
 
 var target_velocity = Vector3.ZERO
 
@@ -38,3 +39,4 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
